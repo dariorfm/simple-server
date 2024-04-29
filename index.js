605092c8e888106a5220d0d398922e7acd9d8e82
dariorfm <dariorfm@gmail.com>
2024-04-29 14:20:23 +0200

    Eslint Standard JS installed and runned

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const Note = require('./models/note');
-
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const Note = require('./models/note')
 
 // Definiendo middleware personalizado
 const requestLogger = (req, res, next) => {
-    console.log('Method:', req.method)
-    console.log('Path:', req.path)
-    console.log('Body:', req.body)
-    console.log('---')
-    next()
+  console.log('Method:', req.method)
+  console.log('Path:', req.path)
+  console.log('Body:', req.body)
+  console.log('---')
+  next()
 }
 
 // Middleware que se usa para servir archivos estáticos
@@ -26,123 +25,114 @@ app.use(express.json())
 // Usando el middleware personalizado tan solo después del middleware que captura solicitudes con datos JSON
 app.use(requestLogger)
 
-
 // Middleware que se usa para permitir solicitudes desde cualquier origen
 app.use(cors())
 
-
 // Datos de prueba o recursos de la API
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        date: "2019-05-30T17:30:31.098Z",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2019-05-30T18:39:34.091Z",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2019-05-30T19:20:14.298Z",
-        important: true
-    }
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
 ]
-
-
 
 // Rutas
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (req, res) => {
-    Note.find({}).then(result => {
-        res.json(result)
-    })
+  Note.find({}).then(result => {
+    res.json(result)
+  })
 })
 
 app.get('/api/notes/:id', (req, res, next) => {
-    Note.findById(req.params.id).then(note => {
-        if (note) {
-            res.json(note)
-        } else {
-            res.status(404).end()
-        }
-    })
+  Note.findById(req.params.id).then(note => {
+    if (note) {
+      res.json(note)
+    } else {
+      res.status(404).end()
+    }
+  })
     .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (req, res, next) => {
-    Note.findByIdAndDelete(req.params.id)
-        .then(result => {
-            res.status(204).end()
-            console.log('This line prints the result parameter:' + result)
-        })
-        .catch(error => next(error))
-})
-
-
-
-app.post('/api/notes', (req, res, next) => {
-    const body = req.body
-    if (body.content === undefined) {
-        return res.status(400).json({error: 'content missing'})
-    }
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-    })
-
-    note.save().then(savedNote => {
-        res.json(savedNote)
+  Note.findByIdAndDelete(req.params.id)
+    .then(result => {
+      res.status(204).end()
+      console.log('This line prints the result parameter:' + result)
     })
     .catch(error => next(error))
-        
 })
 
+app.post('/api/notes', (req, res, next) => {
+  const body = req.body
+  if (body.content === undefined) {
+    return res.status(400).json({ error: 'content missing' })
+  }
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date()
+  })
+
+  note.save().then(savedNote => {
+    res.json(savedNote)
+  })
+    .catch(error => next(error))
+})
 
 app.put('/api/notes/:id', (req, res, next) => {
-    const {content, important} = req.body
+  const { content, important } = req.body
 
-    Note.findByIdAndUpdate(
-        req.params.id, 
-        {content, important},
-        { new: true, runValidators: true, context: 'query' }
-    )
-        .then(updatedNote => {
-            res.json(updatedNote)
-        })
-        .catch(error => next(error))
+  Note.findByIdAndUpdate(
+    req.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedNote => {
+      res.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
-
 
 // Middleware que se usa para captirar solicitudes desconocidas
 // Tener en cuenta que el orden de los middleware es importante
 // Si se coloca al final, capturará todas las solicitudes que no hayan sido capturadas por los middleware anteriores
 // Esta es solo la definición de la función, no se está usando, se usará después de las rutas
 const unknownEndpoint = (req, res) => {
-    res.status(404).send({ error: 'unknown endpoint' })
+  res.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
 // Middleware que se usa para manejar errores
 const errorHandler = (error, req, res, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return res.status(400).send({ error: 'malformatted id' })   
-    } else if (error.name === 'ValidationError') {
-        return res.status(400).json({ error: error.message })
-    }
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 app.use(errorHandler)
@@ -150,5 +140,5 @@ app.use(errorHandler)
 const PORT = process.env.PORT
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- mongo.js
+++ mongo.js
@@ -1,33 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('Give password as an argument');
-    process.exit(1);
+  console.log('Give password as an argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
 const url = `mongodb+srv://dariorfm:${password}@clustera.vtlomcy.mongodb.net/noteApp?retryWrites=true&w=majority&appName=ClusterA` // URL de la base de datos;
 
-mongoose.set('strictQuery', false);
-mongoose.connect(url);
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean
-});
+  content: String,
+  important: Boolean
+})
 
-const Note = mongoose.model('Note', noteSchema);
+const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-    content: process.argv[3],
-    important: process.argv[4]
-});
+  content: process.argv[3],
+  important: process.argv[4]
+})
 
 note.save().then(result => {
-    console.log('note saved!');
-    mongoose.connection.close();
-});
+  console.log('note saved!')
+  mongoose.connection.close()
+})
 
 /* Note.find({}).then(result => {
     result.forEach(note => {
