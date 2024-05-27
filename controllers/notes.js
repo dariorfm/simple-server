8b6ac2b2f38ebd8caf78dd1971353430bd7890c6
dariorfm <dariorfm@gmail.com>
2024-05-27 17:02:04 +0200

    Users management - create new note

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (req, res) => {
   const notes = await Note.find({})
@@ -21,13 +22,19 @@ notesRouter.put('/:id', async (req, res, next) => {
 notesRouter.post('/', async (req, res, next) => {
   const body = req.body
 
+  const user = await User.findById(body.userId)
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date()
+    date: new Date(),
+    user: user._id
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
   res.status(201).json(savedNote)
 })
 
